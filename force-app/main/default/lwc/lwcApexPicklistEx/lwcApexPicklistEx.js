4ee349e5be63786d1c071714f9bf60c05145eb59
Harinadh93 <harinadhbabueluru@gmail.com>
2022-05-20 17:25:57 +0530

    LWC Apex Picklist

---
+++ force-app/main/default/lwc/lwcApexPicklistEx/lwcApexPicklistEx.js
@@ -0,0 +1,21 @@
+import { LightningElement,track, wire } from 'lwc';
+import { getPicklistValues } from 'lightning/uiObjectInfoApi';
+import { getObjectInfo } from 'lightning/uiObjectInfoApi';
+import ACCOUNT_OBJECT from '@salesforce/schema/Account';
+import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
+import Type_FIELD from '@salesforce/schema/Account.Type';
+
+export default class LwcApexPicklistEx extends LightningElement {
+    @track value;
+
+    @wire(getObjectInfo, { objectApiName: ACCOUNT_OBJECT })
+    objectInfo;
+    
+
+    @wire(getPicklistValues, { recordTypeId: '$objectInfo.data.defaultRecordTypeId', fieldApiName: INDUSTRY_FIELD})
+    IndustryPicklistValues;
+
+    handleChange(event) {
+        this.value = event.detail.value;
+    }
+}
